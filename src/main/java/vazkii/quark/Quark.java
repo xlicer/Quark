595b63afc3ead9e3deabb29fd24249f06810bd1d
Vasco Lavos <vazkii@hotmail.com>
2016-03-17 18:37:07 +0000

    It exists

---
+++ src/main/java/vazkii/quark/Quark.java
@@ -0,0 +1,18 @@
+/**
+ * This class was created by <Vazkii>. It's distributed as
+ * part of the Quark Mod. Get the Source Code in github:
+ * https://github.com/Vazkii/Quark
+ * 
+ * Quark is Open Source and distributed under the
+ * [ADD-LICENSE-HERE]
+ * 
+ * File Created @ [17/03/2016, 18:35:05 (GMT)]
+ */
+package vazkii.quark;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid="Quark")
+public class Quark {
+
+}
