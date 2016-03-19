1bbb4ea57dd2e126490789160219a6fb77f4b648
Vasco Lavos <vazkii@hotmail.com>
2016-03-19 17:02:26 +0000

    Moved the angry creeper render out of the class so it doesn't crash servers

--- src/main/java/vazkii/quark/tweaks/feature/AngryCreepers.java
+++ src/main/java/vazkii/quark/tweaks/feature/AngryCreepers.java
@@ -10,42 +10,20 @@
  */
 package vazkii.quark.tweaks.feature;
 
-import org.lwjgl.opengl.GL11;
-
-import net.minecraft.client.renderer.entity.RenderCreeper;
-import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.entity.monster.EntityCreeper;
-import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.client.registry.RenderingRegistry;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 import vazkii.quark.base.module.Feature;
+import vazkii.quark.tweaks.client.render.RenderCreeperAngry;
 
 public class AngryCreepers extends Feature {
 
 	@Override
 	@SideOnly(Side.CLIENT)
 	public void preInitClient(FMLPreInitializationEvent event) {
-		RenderingRegistry.registerEntityRenderingHandler(EntityCreeper.class, manager -> new RenderCreeperRed(manager));
+		RenderingRegistry.registerEntityRenderingHandler(EntityCreeper.class, RenderCreeperAngry.factory());
 	}
-	
-	private static class RenderCreeperRed extends RenderCreeper {
-
-		public RenderCreeperRed(RenderManager renderManagerIn) {
-			super(renderManagerIn);
-		}
-		
-		@Override
-		protected ResourceLocation getEntityTexture(EntityCreeper entity) {
-			if(entity.getCreeperState() == 1) {
-				float antired = 1F - (entity.getCreeperFlashIntensity(0F) / 1.17F + 0.1F);
-				GL11.glColor3f(1F, antired, antired);
-			}
-			return super.getEntityTexture(entity);
-		}
-		
-	}
-	
 	
 }
