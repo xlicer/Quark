e280c9078e14725a4fe831a1fff225d21a66de52
Vasco Lavos <vazkii@hotmail.com>
2016-06-06 00:51:59 +0100

    Changed the way mushrooms generate. Halved the default.

--- src/main/java/vazkii/quark/world/feature/MushroomsInSwamps.java
+++ src/main/java/vazkii/quark/world/feature/MushroomsInSwamps.java
@@ -10,22 +10,54 @@
  */
 package vazkii.quark.world.feature;
 
+import java.util.Random;
+
 import net.minecraft.init.Biomes;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraft.world.World;
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.gen.feature.WorldGenBigMushroom;
+import net.minecraft.world.gen.feature.WorldGenerator;
+import net.minecraftforge.event.terraingen.DecorateBiomeEvent;
+import net.minecraftforge.event.terraingen.DecorateBiomeEvent.Decorate.EventType;
+import net.minecraftforge.fml.common.eventhandler.Event.Result;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import vazkii.quark.base.module.Feature;
 
 public class MushroomsInSwamps extends Feature {
 
-	int bigMushroomsPerChunk;
+    private WorldGenerator bigMushroomGen = new WorldGenBigMushroom();
+	
+	double bigMushroomsPerChunk;
 
 	@Override
 	public void setupConfig() {
-		bigMushroomsPerChunk = loadPropInt("Big mushrooms per chunk", "", 1);
+		bigMushroomsPerChunk = loadPropDouble("Big Mushroom count per chunk", "Must be an integer if above 1. If below 1, works as a chance to generate one per chunk.", 0.5);
 	}
 
+	@SubscribeEvent
+	public void decorate(DecorateBiomeEvent.Decorate event) {
+		World world = event.getWorld();
+		Biome biome = world.getBiomeGenForCoords(event.getPos());
+		Random rand = event.getRand();	
+		
+		if((biome == Biomes.SWAMPLAND || biome == Biomes.MUTATED_SWAMPLAND) && event.getType() == EventType.BIG_SHROOM) {
+			if(bigMushroomsPerChunk < 1 && rand.nextDouble() > bigMushroomsPerChunk)
+				return;
+			
+			int amount = (int) Math.max(1, bigMushroomsPerChunk);
+	        for(int i = 0; i < amount; i++) {
+	            int x = rand.nextInt(16) + 8;
+	            int y = rand.nextInt(16) + 8;
+	            bigMushroomGen.generate(world, rand, world.getHeight(event.getPos().add(x, 0, y)));
+	        }
+	        
+	        event.setResult(Result.DENY);
+		}
+	}
+	
 	@Override
-	public void init(FMLInitializationEvent event) {
-		Biomes.SWAMPLAND.theBiomeDecorator.bigMushroomsPerChunk = bigMushroomsPerChunk;
+	public boolean hasTerrainSubscriptions() {
+		return true;
 	}
 
 }
