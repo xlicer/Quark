090d1c546e48b102ad8288237c077cb31718a092
Vasco Lavos <vazkii@hotmail.com>
2016-06-19 01:58:10 +0100

    Rewrote ASM code to be more flexible and extensible.

--- src/main/java/vazkii/quark/base/asm/ClassTransformer.java
+++ src/main/java/vazkii/quark/base/asm/ClassTransformer.java
@@ -10,8 +10,14 @@
  */
 package vazkii.quark.base.asm;
 
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Map;
+import java.util.function.BiPredicate;
+import java.util.function.Function;
+import java.util.function.Predicate;
 
+import org.apache.commons.lang3.tuple.Pair;
 import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.ClassWriter;
 import org.objectweb.asm.Opcodes;
@@ -27,53 +33,111 @@ import net.minecraftforge.fml.common.FMLLog;
 
 public class ClassTransformer implements IClassTransformer {
 
+	private static final Map<String, Transformer> transformers = new HashMap();
+
+	static {
+		transformers.put("net.minecraft.client.model.ModelBiped", ClassTransformer::transformModelBiped);
+		transformers.put("micdoodle8.mods.galacticraft.core.client.model.ModelPlayerGC", ClassTransformer::transformModelBiped);
+	}
+
 	@Override
 	public byte[] transform(String name, String transformedName, byte[] basicClass) {
-		if(transformedName.equals("net.minecraft.client.model.ModelBiped") || transformedName.equals("micdoodle8.mods.galacticraft.core.client.model.ModelPlayerGC")) {
-			log("Starting on " + transformedName);
-			String funcName = "setRotationAngles";
-			String obfName = "a";
-			String funcDesc = "(FFFFFFLnet/minecraft/entity/Entity;)V";
-			String obfDesc = "(FFFFFFLrr;)V";
+		if(transformers.containsKey(transformedName))
+			return transformers.get(transformedName).apply(basicClass);
 
-			if(LoadingPlugin.runtimeDeobfEnabled)
-				funcName = "func_78087_a";
-			log("Method is " + funcName + " or " + obfName + " for obf.");
-			log("Descriptor is " + funcDesc + " or " + obfDesc + " dor obf.");
+		return basicClass;
+	}
 
-			ClassReader reader = new ClassReader(basicClass);
-			ClassNode node = new ClassNode();
-			reader.accept(node, 0);
+	private static byte[] transformModelBiped(byte[] basicClass) {
+		MethodSignature sig = new MethodSignature("setRotationAngles", "func_78087_a", "a", "(FFFFFFLnet/minecraft/entity/Entity;)V", "(FFFFFFLrr;)V");
 
-			for(MethodNode method : node.methods)
-				if((method.name.equals(funcName)|| method.name.equals(obfName)) && (method.desc.equals(funcDesc) || method.desc.equals(obfDesc))) {
-					log("Found method: " + method.name + " " + method.desc);
-					Iterator<AbstractInsnNode> iterator = method.instructions.iterator();
+		return transform(basicClass, Pair.of(sig, combine(
+				(AbstractInsnNode node) -> { // Filter
+					return node.getOpcode() == Opcodes.RETURN;
+				}, 
+				(MethodNode method, AbstractInsnNode node) -> { // Action
+					InsnList newInstructions = new InsnList();
 
-					while(iterator.hasNext()) {
-						AbstractInsnNode anode = iterator.next();
-						if(anode.getOpcode() == Opcodes.RETURN) {
-							log("RETURN Opcode found");
-							InsnList newInstructions = new InsnList();
+					newInstructions.add(new VarInsnNode(Opcodes.ALOAD, 7));
+					newInstructions.add(new MethodInsnNode(Opcodes.INVOKESTATIC, "vazkii/quark/vanity/client/emotes/base/EmoteHandler", "updateEmotes", "(Lnet/minecraft/entity/Entity;)V"));
 
-							newInstructions.add(new VarInsnNode(Opcodes.ALOAD, 7));
-							newInstructions.add(new MethodInsnNode(Opcodes.INVOKESTATIC, "vazkii/quark/vanity/client/emotes/base/EmoteHandler", "updateEmotes", "(Lnet/minecraft/entity/Entity;)V"));
+					method.instructions.insertBefore(node, newInstructions);
+					return true;
+				})));
+	}
 
-							method.instructions.insertBefore(anode, newInstructions);
-							log("Patched!");
-						}
-					}
+	private static byte[] transform(byte[] basicClass, Pair<MethodSignature, MethodAction>... methods) {
+		ClassReader reader = new ClassReader(basicClass);
+		ClassNode node = new ClassNode();
+		reader.accept(node, 0);
 
-					ClassWriter writer = new ClassWriter(ClassWriter.COMPUTE_MAXS | ClassWriter.COMPUTE_FRAMES);
-					node.accept(writer);
-					return writer.toByteArray();
-				}
+		boolean didAnything = false;
+
+		for(Pair<MethodSignature, MethodAction> pair : methods)
+			didAnything |= findMethodAndTransform(node, pair.getLeft(), pair.getRight());
+
+		if(didAnything) {
+			ClassWriter writer = new ClassWriter(ClassWriter.COMPUTE_MAXS | ClassWriter.COMPUTE_FRAMES);
+			node.accept(writer);
+			return writer.toByteArray();
 		}
 
 		return basicClass;
 	}
 
-	private static void log(String str) {
-		FMLLog.info("[Quark Emotes ASM] %s", str);
+	public static boolean findMethodAndTransform(ClassNode node, MethodSignature sig, MethodAction pred) {
+		String funcName = sig.funcName;
+		if(LoadingPlugin.runtimeDeobfEnabled)
+			funcName = sig.srgName;
+
+		for(MethodNode method : node.methods)
+			if((method.name.equals(funcName)|| method.name.equals(sig.obfName)) && (method.desc.equals(sig.funcDesc) || method.desc.equals(sig.obfDesc)))
+				return pred.test(method);
+
+		return false;
 	}
+
+	public static MethodAction combine(NodeFilter filter, NodeAction action) {
+		return (MethodNode mnode) -> applyOnNode(mnode, filter, action);
+	}
+
+	public static boolean applyOnNode(MethodNode method, NodeFilter filter, NodeAction action) {
+		Iterator<AbstractInsnNode> iterator = method.instructions.iterator();
+
+		boolean didAny = false;
+		while(iterator.hasNext()) {
+			AbstractInsnNode anode = iterator.next();
+			if(filter.test(anode)) {
+				didAny = true;
+				if(action.test(method, anode))
+					return true;	
+			}
+		}
+
+		return false;
+	}
+
+	private static void log(String str) {
+		FMLLog.info("[Quark ASM] %s", str);
+	}
+
+	private static class MethodSignature {
+		String funcName, srgName, obfName, funcDesc, obfDesc;
+
+		public MethodSignature(String funcName, String srgName, String obfName, String funcDesc, String obfDesc) {
+			this.funcName = funcName;
+			this.srgName = srgName;
+			this.obfName = obfName;
+			this.funcDesc = funcDesc;
+			this.obfDesc = obfName;
+		}
+
+	}
+	
+	// Basic interface aliases to not have to clutter up the code with generics over and over again
+	private static interface Transformer extends Function<byte[], byte[]> { }
+	private static interface MethodAction extends Predicate<MethodNode> { }
+	private static interface NodeFilter extends Predicate<AbstractInsnNode> { }
+	private static interface NodeAction extends BiPredicate<MethodNode, AbstractInsnNode> { }
+	
 }
