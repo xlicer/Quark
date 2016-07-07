3f35201ec185b7f4952a22bf9642a5ba3b796f1b
Vasco Lavos <vazkii@hotmail.com>
2016-07-07 19:49:04 +0100

    Autojump HUD.

--- src/main/java/vazkii/quark/tweaks/feature/AutoJumpHotkey.java
+++ src/main/java/vazkii/quark/tweaks/feature/AutoJumpHotkey.java
@@ -10,13 +10,23 @@
  */
 package vazkii.quark.tweaks.feature;
 
+import org.lwjgl.opengl.GL11;
+
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Gui;
+import net.minecraft.client.gui.ScaledResolution;
+import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.settings.GameSettings.Options;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.text.TextComponentTranslation;
 import net.minecraft.client.settings.KeyBinding;
+import net.minecraftforge.client.event.RenderGameOverlayEvent;
+import net.minecraftforge.client.event.RenderGameOverlayEvent.ElementType;
 import net.minecraftforge.fml.client.registry.ClientRegistry;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent.ClientTickEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent.Phase;
 import net.minecraftforge.fml.common.gameevent.TickEvent.RenderTickEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
@@ -24,29 +34,70 @@ import vazkii.quark.base.module.Feature;
 
 public class AutoJumpHotkey extends Feature {
 
+	private static final float DISPLAY_TIME = 10F;
+	private static final float FADE_TIME = 10F;
+	private static final float TOTAL_TIME = DISPLAY_TIME + FADE_TIME;
+	
+	private static final ResourceLocation AUTOJUMP_OFF = new ResourceLocation("quark", "textures/misc/autojump_off.png");
+	private static final ResourceLocation AUTOJUMP_ON = new ResourceLocation("quark", "textures/misc/autojump_on.png");
+	
+	@SideOnly(Side.CLIENT)
 	KeyBinding key;
+	
 	boolean down;
+	int hudTime;
 	
 	@Override
+	@SideOnly(Side.CLIENT)
 	public void preInitClient(FMLPreInitializationEvent event) {
 		key = new KeyBinding("quark.keybind.toggleAutojump", 48, "key.categories.movement");
 		ClientRegistry.registerKeyBinding(key);
 	}
 	
-	
 	@SubscribeEvent
 	@SideOnly(Side.CLIENT)
-	public void onKeyInput(RenderTickEvent event) {
+	public void onRenderTick(RenderTickEvent event) {
 		Minecraft mc = Minecraft.getMinecraft();
 		boolean downNow = key.isKeyDown();
 		if(mc.inGameHasFocus && !down && downNow) {
 			boolean b = mc.gameSettings.getOptionOrdinalValue(Options.AUTO_JUMP);
 			int i = b ? 1 : 0;
 			mc.gameSettings.setOptionValue(Options.AUTO_JUMP, ~i & 1);
-			mc.thePlayer.addChatComponentMessage(new TextComponentTranslation("quark.autojump" + i));
+			hudTime = (int) TOTAL_TIME;
 		}
 		
 		down = downNow;
+	}
+	
+	@SubscribeEvent
+	@SideOnly(Side.CLIENT)
+	public void onClientTick(ClientTickEvent event) {
+		if(event.phase == Phase.END && hudTime > 0)
+			hudTime--;
+	}
+	
+	@SubscribeEvent
+	@SideOnly(Side.CLIENT)
+	public void onRenderOverlay(RenderGameOverlayEvent.Post event) {
+		if(event.getType() != ElementType.ALL || hudTime == 0)
+			return;
+		
+		Minecraft mc = Minecraft.getMinecraft();
+		ScaledResolution res = event.getResolution();
+		int x = res.getScaledWidth() / 2 - 16;
+		int y = res.getScaledHeight() - 100;
+		boolean autojumpEnabled = mc.gameSettings.getOptionOrdinalValue(Options.AUTO_JUMP);
+
+		float calcTime = Math.min(FADE_TIME, hudTime + DISPLAY_TIME);
+		float alpha = (hudTime + event.getPartialTicks()) / FADE_TIME;
+		
+		GlStateManager.pushMatrix();
+		GlStateManager.enableBlend();
+		GlStateManager.blendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+		GlStateManager.color(1F, 1F, 1F, alpha);
+		mc.renderEngine.bindTexture(autojumpEnabled ? AUTOJUMP_ON : AUTOJUMP_OFF);
+		Gui.drawModalRectWithCustomSizedTexture(x, y, 0, 0, 32, 32, 32, 32);
+		GlStateManager.popMatrix();
 	}
 	
 	@Override
