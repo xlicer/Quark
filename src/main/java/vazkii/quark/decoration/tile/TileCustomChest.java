5f13a78502b129031e4efee1f80bc64f4c874601
Vasco Lavos <vazkii@hotmail.com>
2016-07-12 22:15:40 +0100

    Deny refreshing the chest TE save for a hard block change. Closes #116

--- src/main/java/vazkii/quark/decoration/tile/TileCustomChest.java
+++ src/main/java/vazkii/quark/decoration/tile/TileCustomChest.java
@@ -14,14 +14,19 @@ import javax.annotation.Nullable;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockChest;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.network.NetworkManager;
 import net.minecraft.network.play.server.SPacketUpdateTileEntity;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.tileentity.TileEntityChest;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.util.datafix.DataFixer;
+import net.minecraft.util.datafix.FixTypes;
+import net.minecraft.util.datafix.walkers.ItemStackDataLists;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import vazkii.quark.decoration.feature.VariedChests;
 
 public class TileCustomChest extends TileEntityChest {
@@ -46,6 +51,11 @@ public class TileCustomChest extends TileEntityChest {
 	public void handleUpdateTag(NBTTagCompound tag) {
 		super.handleUpdateTag(tag);
 		chestType = VariedChests.ChestType.getType(tag.getString("type"));
+	}
+
+	@Override
+	public boolean shouldRefresh(World world, BlockPos pos, IBlockState oldState, IBlockState newState) {
+		return oldState.getBlock() != newState.getBlock();
 	}
 
 	@Nullable
