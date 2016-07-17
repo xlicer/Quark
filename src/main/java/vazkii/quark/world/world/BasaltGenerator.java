eaae847210ee1c498f2c43305840e6842618976c
Vasco Lavos <vazkii@hotmail.com>
2016-07-17 05:42:46 +0100

    Use dimension type for the basalt generator. Closes #120

--- src/main/java/vazkii/quark/world/world/BasaltGenerator.java
+++ src/main/java/vazkii/quark/world/world/BasaltGenerator.java
@@ -15,6 +15,7 @@ import java.util.Random;
 import net.minecraft.block.state.pattern.BlockMatcher;
 import net.minecraft.init.Blocks;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.DimensionType;
 import net.minecraft.world.World;
 import net.minecraft.world.WorldProviderHell;
 import net.minecraft.world.WorldProviderSurface;
@@ -43,8 +44,8 @@ public class BasaltGenerator implements IWorldGenerator {
 
 	@Override
 	public void generate(Random rand, int chunkX, int chunkZ, World world, IChunkGenerator chunkGenerator, IChunkProvider chunkProvider) {
-		boolean isNether = world.provider instanceof WorldProviderHell;
-		boolean isOverworld = world.provider instanceof WorldProviderSurface;
+		boolean isNether = world.provider.getDimensionType() == DimensionType.NETHER;
+		boolean isOverworld = world.provider.getDimensionType() == DimensionType.OVERWORLD;
 
 		if(isNether && !nether || isOverworld && !overworld || !isNether && !isOverworld)
 			return;
