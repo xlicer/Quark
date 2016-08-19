67d5234ac43f1c9145d6542de55d9fa23f1da00a
Vasco Lavos <vazkii@hotmail.com>
2016-08-19 15:59:28 +0100

    Removed Tabards. They weren't going anywhere anyway. Closes #159

--- src/main/java/vazkii/quark/experimental/QuarkExperimental.java
+++ src/main/java/vazkii/quark/experimental/QuarkExperimental.java
@@ -11,18 +11,15 @@
 package vazkii.quark.experimental;
 
 import vazkii.quark.base.module.Module;
-import vazkii.quark.experimental.command.CommandConfig;
 import vazkii.quark.experimental.features.BiggerCaves;
 import vazkii.quark.experimental.features.ConfigCommand;
 import vazkii.quark.experimental.features.PrintSuppressor;
-import vazkii.quark.experimental.features.Tabards;
 
 public class QuarkExperimental extends Module {
 
 	@Override
 	public void addFeatures() {
 		registerFeature(new BiggerCaves(), false);
-		registerFeature(new Tabards(), false);
 		registerFeature(new PrintSuppressor(), false);
 		registerFeature(new ConfigCommand(), false);
 	}
