99ae8eff80289a80edc082ee09fe9a8023cb39a3
Vasco Lavos <vazkii@hotmail.com>
2016-12-03 23:40:14 +0000

    Verify stack NBT existance before attempting to get it for tooltip drawing.

--- src/main/java/vazkii/quark/vanity/feature/DyeItemNames.java
+++ src/main/java/vazkii/quark/vanity/feature/DyeItemNames.java
@@ -25,7 +25,7 @@ public class DyeItemNames extends Feature {
 	@SideOnly(Side.CLIENT)
 	public void makeTooltip(ItemTooltipEvent event) {
 		ItemStack stack = event.getItemStack(); 
-		if(stack != null && ItemNBTHelper.getInt(stack, TAG_DYE, -1) != -1) {
+		if(stack != null && stack.hasTagCompound() && ItemNBTHelper.getInt(stack, TAG_DYE, -1) != -1) {
 			FontRenderer font = Minecraft.getMinecraft().fontRendererObj; 
 			int len = font.getStringWidth(stack.getDisplayName());
 			String spaces = "";
@@ -40,7 +40,7 @@ public class DyeItemNames extends Feature {
 	@SideOnly(Side.CLIENT)
 	public void renderTooltip(RenderTooltipEvent.PostText event) {
 		ItemStack stack = event.getStack(); 
-		if(stack != null) {
+		if(stack != null && stack.hasTagCompound()) {
 			int dye = ItemNBTHelper.getInt(stack, TAG_DYE, -1);
 			if(dye != -1) {
 				int rgb = ItemDye.DYE_COLORS[Math.min(15, dye)];
