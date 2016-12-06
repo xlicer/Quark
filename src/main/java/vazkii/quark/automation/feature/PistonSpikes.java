3648edeabedbafc00edbb7a5ff92ce0dc5da500d
Vasco Lavos <vazkii@hotmail.com>
2016-12-06 18:16:50 +0000

    Fixed another slime block dupe.

--- src/main/java/vazkii/quark/automation/feature/PistonSpikes.java
+++ src/main/java/vazkii/quark/automation/feature/PistonSpikes.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockDirectional;
+import net.minecraft.block.BlockPistonBase;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.init.Blocks;
 import net.minecraft.util.EnumFacing;
@@ -27,26 +28,31 @@ public class PistonSpikes extends Feature {
 		for(BlockPos pos : moveList) {
 			IBlockState state = world.getBlockState(pos);
 			if(state.getBlock() == Blocks.END_ROD && state.getValue(BlockDirectional.FACING) == facing) {
-				BlockPos off = pos.offset(facing);
+				BlockPos off = pos.offset(facing.getOpposite());
 				
-				for(int i = 0; i < 12; i++) {
+				for(int i = 0; i < 14; i++) {
 					IBlockState stateAt = world.getBlockState(off);
 					Block blockAt = stateAt.getBlock();
 					if(blockAt.isAir(stateAt, world, off))
 						break;
 					
+					if(i == 0 && blockAt != Blocks.PISTON && blockAt != Blocks.STICKY_PISTON)
+						return false;
+					
 					if(blockAt == Blocks.SLIME_BLOCK)
 						return false;
 					
-					if(i == 0) {
-						newDestroyList.add(off);
-						if(newMoveList.contains(off))
-							newMoveList.remove(off);
-					} else {
-						if(newMoveList.contains(off))
-							newMoveList.remove(off);
-						else if(newDestroyList.contains(off))
-							newDestroyList.remove(off);
+					if(i >= 2) {
+						if(i == 2) {
+							newDestroyList.add(off);
+							if(newMoveList.contains(off))
+								newMoveList.remove(off);
+						} else {
+							if(newMoveList.contains(off))
+								newMoveList.remove(off);
+							else if(newDestroyList.contains(off))
+								newDestroyList.remove(off);
+						}
 					}
 
 					off = off.offset(facing);
